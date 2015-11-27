aec04015a97137c4597a92b8dbf66c10dc5d172d
krishna rokhale <krishna.rokhale@gmail.com>
2015-11-27 10:49:35 +0530

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VishwasInterview::Application.config.secret_token = '3895eec3324fa60b86fc764e5b43f1ccfc55b4aaa08c6ce2ce8646add402a7934bf706ee7717449e0f773ca70c8d5c802e17bb5e07519eda3a06058d64b9a448'
